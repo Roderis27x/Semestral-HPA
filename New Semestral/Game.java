fccb3b3e1dabc191ef1c377eca0d6811c3022a4e
Roderis Ortega <78380781+Roderis0927@users.noreply.github.com>
2022-07-03 21:49:34 -0500

    Update Game.java

--- New Semestral/Game.java
+++ New Semestral/Game.java
@@ -6,7 +6,7 @@ public class Game {
     int gameStatus; //Estado del juego
     int turn; //Turno del jugador
     String action;  //Acciones del jugador
-    String move; //Movimiento del jugador en el tablero
+    String move; //Movimiento del jugador en el tablero}
     Figuras caballoBlanco, caballoNegro; //Caballos del juego
     Board board; //Tablero del juego
 
@@ -24,25 +24,82 @@ public class Game {
 
     } //Fin de PrepareGame
 
+    public void ganador(Figuras ca, String nCaballo){
+
+        System.out.print(nCaballo + " " + "Ha ganado"); //Imprime el mensaje de ganador
+        System.out.print("El recorrido es: " + ca.posicion); //Imprime el recorrido del caballo
+        System.out.print("El turno de jugo es: " + turn); //Imprime el turno del jugador
+
+    } //Fin de ganador
+
     public void GameStart(){
         //GameStatus = 0: Salir de juego
         //GameStatus = 1: Turno del jugador 1 (Caballo Blanco)
         //GameStatus = 2: Turno del jugaador 2 (Caballo Negro)
 
-        while(gameStatus!=0){ 
+        while(gameStatus != 0){ 
 
-            if(gameStatus==1){ 
+            if(gameStatus == 1){ 
 
-                System.out.print("El turno del jugador: " + turn);
-                board.PrintBoard();
+                System.out.print("El turno del jugador: " + turn); //Se muestra el turno del jugador
+                board.PrintBoard(); //Se imprime el tablero
 
-                System.out.print("Turno del Caballo Blanco: Mover o Salir...");
-                System.out.print("La posicion es: " + caballoBlanco.posc);
+                System.out.print("Turno del Caballo Blanco: Mover o Salir..."); //Se muestra el turno del jugador
+                System.out.print("La posicion es: " + caballoBlanco.posc); //Se muestra la posicion del caballo
+                try {
 
+                    action = read.nextLine(); //Se lee la accion del jugador
 
+                } catch (Exception e) {
 
+                    System.out.println("Error... Ingrese una accion valida"); //Se muestra el error
 
+                }
+
+                if(action.equals("mover") || (action.equals("Mover") || action.equals("MOVER") )){
+
+                    System.out.print("Ingrese la siguiente posicion: "); //Se muestra el mensaje de ingreso de la siguiente posicion
+                    move = read.nextLine(); //Se lee la siguiente posicion del jugador
+                    if(caballoBlanco.posc.equals(caballoNegro.posc)){
+                        ganador(caballoBlanco, "Caballo Blanco"); //Se imprime el mensaje de ganador
+                        gameStatus = 0; //Se cambia el estado del juego a 0
+                    }
+
+                    gameStatus = 2; //Se cambia el estado del juego a 2
+
+                } //Fin del if
+                else if(action.equals("salir") || (action.equals("Salir") || action.equals("SALIR") )){
+                    ganador(caballoNegro, "Caballo Negro"); //Se imprime el mensaje de ganador
+                    gameStatus = 0; //Se cambia el estado del juego a 0
+        
+                } //Fin del else if
+                else{
+
+                    System.out.println("Error... Ingrese una accion valida"); //Se muestra el error
+
+                } //Fin del else
+                   
+            } //Fin del if
+            else{ 
+                
+                System.out.print("Turno del Caballo Negro: Mover o Salir...");
+                System.out.print("La posicion es: " + caballoNegro);
+
+                try {
+
+                    action = read.nextLine();
+
+                } catch (Exception e) {
+                    
+                    System.out.println("Error... Ingrese una accion valida"); //Se muestra el error
+                }
             }
+                
+               
+        
+
+
+
 
         } //Fin While
 
